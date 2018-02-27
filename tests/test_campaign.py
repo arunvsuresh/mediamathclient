530af1944d556447a4febef535af42644ab7173f
vagrant <vagrant@vagrant.vm>
2018-02-27 23:35:05 +0000

    changed assertTrue to assertIn for 2nd test

--- tests/test_campaign.py
+++ tests/test_campaign.py
@@ -6,9 +6,12 @@ import json
 class TestMediaMathCampaign(TestCase):
 
   def test_connection(self):
-    session_id = campaign.Campaign().get_connection()
+    c = campaign.Campaign()
+    session_id = c.get_connection()
     self.assertIsNotNone(session_id)
 
   def test_get_campaigns(self):
-    campaigns = campaign.Campaign().get_campaigns()
-    self.assertTrue('"msg_type": "success"', campaigns)
+    c = campaign.Campaign()
+    campaigns = c.get_campaigns()
+    self.assertIn('"msg_type": "success"', campaigns)
+
