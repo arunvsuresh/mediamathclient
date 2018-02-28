fa91e423b4730f34c0341f65812be99fa58952d7
vagrant <arunvsuresh@gmail.com>
2018-02-28 20:35:12 +0000

    refactor xml to json process some more

--- mediamathclient/campaign.py
+++ mediamathclient/campaign.py
@@ -38,11 +38,7 @@ class Campaign:
 
     return response_json
 
-  def get_campaigns_by_advertiser(self, advertiser_id):
-    t1 = self.get_connection()
-    url = t1._construct_url("campaigns", entity=None, child=None, limit={"advertiser": int(advertiser_id)})[0]
-    url = "https://" + t1.api_base + "/" + t1._get_service_path('campaigns') + "/" + url
-    response = t1.session.get(url)
+  def convert_xml_to_json(self, response, url):
     # convert headers to real dict since it comes back as CaseInsensitiveDict
     headers = dict(response.headers)
     # get response content, comes back as xml
@@ -52,6 +48,14 @@ class Campaign:
     # convert json string back to json dict to create proper json response
     json_dict = json.loads(json_string)
     request_body = url, headers
+    return json_dict, request_body
+
+  def get_campaigns_by_advertiser(self, advertiser_id):
+    t1 = self.get_connection()
+    url = t1._construct_url("campaigns", entity=None, child=None, limit={"advertiser": int(advertiser_id)})[0]
+    url = "https://" + t1.api_base + "/" + t1._get_service_path('campaigns') + "/" + url
+    response = t1.session.get(url)
+    json_dict, request_body = self.convert_xml_to_json(response, url)
     response_json = self.generate_json_response("campaigns", json_dict, response, request_body)
     return json.dumps(response_json)
 
@@ -60,15 +64,7 @@ class Campaign:
     url = t1._construct_url("campaigns", entity=campaign_id, child=None, limit=None)[0]
     url = "https://" + t1.api_base + "/" + t1._get_service_path('campaigns') + "/" + url
     response = t1.session.get(url)
-    # convert headers to real dict since it comes back as CaseInsensitiveDict
-    headers = dict(response.headers)
-    # get response content, comes back as xml
-    xml_string = response.content
-    # convert xml to json string and remove unwanted "@" symbol
-    json_string = json.dumps(xmltodict.parse(xml_string), indent=4).replace(chr(64), '')
-    # convert json string back to json dict to create proper json response
-    json_dict = json.loads(json_string)
-    request_body = url, headers
+    json_dict, request_body = self.convert_xml_to_json(response, url)
     response_json = self.generate_json_response("campaign", json_dict, response, request_body)
     return json.dumps(response_json)
 
@@ -77,13 +73,6 @@ class Campaign:
     url = t1._construct_url("campaigns", entity=None, child=None, limit=None)[0]
     url = "https://" + t1.api_base + "/" + t1._get_service_path('campaigns') + "/" + url
     response = t1.session.post(url, payload)
-    headers = dict(response.headers)
-    # get response content, comes back as xml
-    xml_string = response.content
-    # convert xml to json string and remove unwanted "@" symbol
-    json_string = json.dumps(xmltodict.parse(xml_string), indent=4).replace(chr(64), '')
-    # convert json string back to json dict to create proper json response
-    json_dict = json.loads(json_string)
-    request_body = url, headers
+    json_dict, request_body = self.convert_xml_to_json(response, url)
     response_json = self.generate_json_response("campaign", json_dict, response, request_body)
     return json.dumps(response_json)
