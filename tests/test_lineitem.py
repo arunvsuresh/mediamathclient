48b901e8f55cf32a3b1c1d1584d96ca4ff98ac26
vagrant <arunvsuresh@gmail.com>
2018-03-02 22:33:42 +0000

    better variables names for lineitem

--- tests/test_lineitem.py
+++ tests/test_lineitem.py
@@ -6,24 +6,24 @@ import json
 class TestMediaMathLineItem(TestCase):
 
   def test_connection(self):
-    li = lineitem.get_connection()
-    session_id = li.session_id
+    line_item = lineitem.get_connection()
+    session_id = line_item.session_id
     self.assertIsNotNone(session_id)
 
   def test_get_lineitem_by_id(self):
-    c = lineitem.LineItem()
-    c = c.get_lineitem_by_id(1188752)
-    c = json.loads(c)
-    self.assertIn(c['msg_type'], 'success')
+    line_item = lineitem.LineItem()
+    line_item = line_item.get_lineitem_by_id(1188752)
+    line_item = json.loads(line_item)
+    self.assertIn(line_item['msg_type'], 'success')
 
   def test_get_lineitems_by_campaign(self):
-    li = lineitem.LineItem()
-    li = li.get_lineitems_by_campaign(243821)
-    li = json.loads(li)
-    self.assertIn(li['msg_type'], 'success')
+    line_item = lineitem.LineItem()
+    line_item = line_item.get_lineitems_by_campaign(243821)
+    line_item = json.loads(line_item)
+    self.assertIn(line_item['msg_type'], 'success')
 
   def test_create_lineitem(self):
-    c = lineitem.LineItem()
+    line_item = lineitem.LineItem()
 
     data = {
       'campaign_id': 243821,
@@ -36,13 +36,13 @@ class TestMediaMathLineItem(TestCase):
       'use_optimization': 0,
       'type': 'GBO',
     }
-    new_lineitem = c.create_lineitem(data)
+    new_lineitem = line_item.create_lineitem(data)
     new_lineitem = json.loads(new_lineitem)
     self.assertIn(new_lineitem['msg_type'], 'success')
 
   # test when missing required field (campaign_id in this case)
   def test_error_in_create_lineitem(self):
-    c = lineitem.LineItem()
+    line_item = lineitem.LineItem()
 
     data = {
       'name': 'arun test line item',
@@ -54,6 +54,6 @@ class TestMediaMathLineItem(TestCase):
       'use_optimization': 0,
       'type': 'GBO',
     }
-    new_lineitem = c.create_lineitem(data)
+    new_lineitem = line_item.create_lineitem(data)
     new_lineitem = json.loads(new_lineitem)
     self.assertIn(new_lineitem['msg_type'], 'error')
