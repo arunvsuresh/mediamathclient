b340b823be1542484a8cf18c41ade2486ee666e8
vagrant <arunvsuresh@gmail.com>
2018-03-22 18:48:27 +0000

    create base class which contains all generic methods for each MM object + modify campaign tests to reflect new refactor

--- mediamathclient/campaign.py
+++ mediamathclient/campaign.py
@@ -1,100 +1,19 @@
-import json
-import requests
-import os
-import terminalone
-import datetime
+from base import Base
 
-# connect to t1
-def get_connection():
-  creds = {
-    "username": os.environ['MM_USERNAME'],
-    "password": os.environ['MM_PASSWORD'],
-    "api_key": os.environ['MM_API_KEY']
-  }
-  return terminalone.T1(auth_method="cookie", **creds)
+class Campaign(Base):
 
-class Campaign:
+  obj_name = 'campaigns'
 
-  t1 = get_connection()
-  base_url = "https://" + t1.api_base + "/"
-  service_url = t1._get_service_path('campaigns') + "/"
-  constructed_url = t1._construct_url("campaigns", entity=None, child=None, limit=None)[0]
-  url = base_url + service_url + constructed_url
-  headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/vnd.mediamath.v1+json',
-             'Cookie': 'adama_session=' + str(t1.session_id)}
-
-  def __init__(self, data=None, omg_campaign=None):
-
-    self.data = data
-    self.omg_campaign = omg_campaign
-
-  def error_check_json_response(self, response_json, json_dict):
-    # error checking
-    if 'errors' in json_dict:
-      response_json['msg_type'] = 'error'
-      response_json['msg'] = json_dict['errors']
-      response_json['data'] = json_dict['errors']
-
-    else:
-      response_json['data'] = json_dict['data']
-      response_json['msg_type'] = 'success'
-      response_json['msg'] = ''
-
-    return response_json
-
-  def generate_json_response(self, json_dict, response, request_body):
-    response_json = {
-      "response_code": response.status_code,
-      "request_body": request_body
-    }
-
-    response_json = self.error_check_json_response(response_json, json_dict)
-    return response_json
-
-  def generate_url(self):
-    base_url = "https://" + self.t1.api_base + "/"
-    service_url = self.t1._get_service_path('campaigns') + "/"
-    constructed_url = self.t1._construct_url("campaigns", entity=None, child=None, limit=None)[0]
-    url = base_url + service_url + constructed_url
-    return url
-
-  def get_campaign_by_id(self, campaign_id):
-    campaign_id = int(campaign_id)
-    url = self.generate_url() + "/" + str(campaign_id)
-    return self.call_mm_api('GET', url)
+  # def __init__(self, data=None, omg_campaign=None):
+  #   self.data = data
+  #   self.omg_campaign = omg_campaign
 
   def get_campaigns_by_advertiser(self, advertiser_id):
     advertiser_id = int(advertiser_id)
     url = self.generate_url() + "/limit/advertiser={0}".format(advertiser_id)
     return self.call_mm_api('GET', url)
 
-  def create_campaign(self, payload):
-    url = self.generate_url()
-    return self.call_mm_api('POST', url, payload)
-
-  # updates existing campaigns
-  def update_campaign(self, payload, campaign_id):
-    campaign_id = int(campaign_id)
-    url = self.generate_url() + "/" + str(campaign_id)
-    return self.call_mm_api('POST', url, payload)
-
   def get_budget_flights(self, campaign_id):
     campaign_id = int(campaign_id)
     url = self.generate_url() + "/" + str(campaign_id) + "/budget_flights?full=*"
     return self.call_mm_api('GET', url)
-
-  def call_mm_api(self, obj_type, url, data=None):
-    if obj_type == 'GET':
-      response = requests.get(url, headers=self.headers)
-      json_dict = response.json()
-      request_body = url, self.headers
-      response_json = self.generate_json_response(json_dict, response, request_body)
-      return json.dumps(response_json)
-
-    if obj_type == 'POST':
-      response = requests.post(url, headers=self.headers, data=data)
-      json_dict = response.json()
-      request_body = url, self.headers
-      response_json = self.generate_json_response(json_dict, response, request_body)
-      return json.dumps(response_json)
-
--- tests/test_campaign.py
+++ tests/test_campaign.py
@@ -5,106 +5,107 @@ import json
 
 class TestMediaMathCampaign(TestCase):
 
-  def test_connection(self):
-    c = campaign.get_connection()
-    session_id = c.session_id
-    self.assertIsNotNone(session_id)
+    def test_connection(self):
+        campaign_instance = campaign.Campaign()
+        session_id = campaign_instance.t1.session_id
+        self.assertIsNotNone(session_id)
 
-  def test_get_campaigns_by_advertiser(self):
-    c = campaign.Campaign()
-    campaigns = c.get_campaigns_by_advertiser(100429)
-    c = json.loads(campaigns)
-    self.assertIn(c['msg_type'], 'success')
+    def test_get_campaign_by_id(self):
+        c = campaign.Campaign()
+        c = c.get_by_id(483494)
+        c = json.loads(c)
+        self.assertIn(c['msg_type'], 'success')
 
-  def test_get_campaign_by_id(self):
-    c = campaign.Campaign()
-    c = c.get_campaign_by_id(483494)
-    c = json.loads(c)
-    self.assertIn(c['msg_type'], 'success')
+    def test_get_campaigns_by_advertiser(self):
+        c = campaign.Campaign()
+        campaigns = c.get_campaigns_by_advertiser(100429)
+        c = json.loads(campaigns)
+        self.assertIn(c['msg_type'], 'success')
 
-  def test_create_campaign(self):
-    c = campaign.Campaign()
+    def test_create_campaign(self):
+        c = campaign.Campaign()
 
-    data = {
-      'advertiser_id': 162259,
-      'ad_server_id': 9,
-      'end_date': '2018-03-22T09:00:00',
-      'goal_type': 'spend',
-      'goal_value': '0.0001',
-      'name': 'arun test campaign 03/19',
-      'service_type': 'SELF',
-      'start_date': '2018-03-20T23:59:00',
-      'total_budget': 2
-    }
-    new_campaign = c.create_campaign(data)
-    new_campaign = json.loads(new_campaign)
-    self.assertIn(new_campaign['msg_type'], 'success')
+        data = {
+          'advertiser_id': 162259,
+          'ad_server_id': 9,
+          'end_date': '2018-03-26T09:00:00',
+          'goal_type': 'spend',
+          'goal_value': '0.0001',
+          'name': 'arun test campaign 03/22',
+          'service_type': 'SELF',
+          'start_date': '2018-03-22T23:59:00',
+          'total_budget': 2
+        }
+        new_campaign = c.create(data)
+        new_campaign = json.loads(new_campaign)
+        self.assertIn(new_campaign['msg_type'], 'success')
 
-  # test when missing required field (advertiser_id in this case)
-  def test_error_in_create_campaign(self):
-    c = campaign.Campaign()
+    # test when missing required field (advertiser_id in this case)
+    def test_error_in_create_campaign(self):
+        c = campaign.Campaign()
 
-    data = {
-      'ad_server_id': 9,
-      'end_date': '2018-03-10T09:00:00+0000',
-      'goal_type': 'spend',
-      'goal_value': '0.0001',
-      'name': 'arun test campaign 03/02/2018',
-      'service_type': 'SELF',
-      'start_date': '2018-03-02T23:59:00+0000',
-      'total_budget': 2
-    }
-    new_campaign = c.create_campaign(data)
-    new_campaign = json.loads(new_campaign)
-    self.assertIn(new_campaign['msg_type'], 'error')
+        data = {
+          'ad_server_id': 9,
+          'end_date': '2018-03-10T09:00:00+0000',
+          'goal_type': 'spend',
+          'goal_value': '0.0001',
+          'name': 'arun test campaign 03/02/2018',
+          'service_type': 'SELF',
+          'start_date': '2018-03-02T23:59:00+0000',
+          'total_budget': 2
+        }
+        new_campaign = c.create(data)
+        new_campaign = json.loads(new_campaign)
+        self.assertIn(new_campaign['msg_type'], 'error')
 
-  def test_update_campaign(self):
-    # initialize campaign instance
-    c = campaign.Campaign()
-    # get campaign by id
-    # old_campaign = c.get_campaign_by_id(485277)
-    # # # convert json str to json dict
-    # old_campaign = json.loads(old_campaign)
-    # del old_campaign['data']['id']
-    # del old_campaign['data']['initial_start_date']
-    # del old_campaign['data']['created_on']
-    # del old_campaign['data']['updated_on']
-    # del old_campaign['data']['entity_type']
-    # del old_campaign['data']['goal_value']
-    # del old_campaign['data']['use_mm_freq']
-    # del old_campaign['data']['dcs_data_is_campaign_level']
-    # del old_campaign['data']['frequency_optimization']
-    # del old_campaign['data']['has_custom_attribution']
-    # del old_campaign['data']['impression_cap_automatic']
-    # del old_campaign['data']['minimize_multi_ads']
-    # del old_campaign['data']['override_suspicious_traffic_filter']
-    # del old_campaign['data']['restrict_targeting_to_deterministic_id']
-    # del old_campaign['data']['restrict_targeting_to_same_device_id']
-    # del old_campaign['data']['spend_cap_automatic']
-    # del old_campaign['data']['total_budget']
-    # del old_campaign['data']['status']
-    # del old_campaign['data']['use_default_ad_server']
+  # def test_update_campaign(self):
+  #   # initialize campaign instance
+  #   c = campaign.Campaign()
+  #   # get campaign by id
+  #   # old_campaign = c.get_campaign_by_id(485277)
+  #   # # # convert json str to json dict
+  #   # old_campaign = json.loads(old_campaign)
+  #   # del old_campaign['data']['id']
+  #   # del old_campaign['data']['initial_start_date']
+  #   # del old_campaign['data']['created_on']
+  #   # del old_campaign['data']['updated_on']
+  #   # del old_campaign['data']['entity_type']
+  #   # del old_campaign['data']['goal_value']
+  #   # del old_campaign['data']['use_mm_freq']
+  #   # del old_campaign['data']['dcs_data_is_campaign_level']
+  #   # del old_campaign['data']['frequency_optimization']
+  #   # del old_campaign['data']['has_custom_attribution']
+  #   # del old_campaign['data']['impression_cap_automatic']
+  #   # del old_campaign['data']['minimize_multi_ads']
+  #   # del old_campaign['data']['override_suspicious_traffic_filter']
+  #   # del old_campaign['data']['restrict_targeting_to_deterministic_id']
+  #   # del old_campaign['data']['restrict_targeting_to_same_device_id']
+  #   # del old_campaign['data']['spend_cap_automatic']
+  #   # del old_campaign['data']['total_budget']
+  #   # del old_campaign['data']['status']
+  #   # del old_campaign['data']['use_default_ad_server']
+  #
+  #
+  #
+  #   # get campaign id to pass into save()
+  #   # campaign_id = old_campaign['data']['id']
+  #   data = {
+  #     'advertiser_id': 162259,
+  #     'ad_server_id': 9,
+  #     # # 'end_date': '2018-03-12T09:00:00',
+  #     # 'goal_type': 'spend',
+  #     # 'goal_value': '0.0001',
+  #     'name': 'test campaign on 03/07/2018',
+  #     # 'service_type': 'SELF',
+  #     # # 'start_date': '2018-03-11T23:59:00',
+  #     # 'total_budget': 2,
+  #   }
+  #
+  #   updated_campaign = c.update_campaign(data, 485277)
+  #
+  #   # self.assertIn(json.loads(updated_campaign)['data']['name'], 'this is arun suresh\'s test campaign on 03/06/2018')
 
-
-
-    # get campaign id to pass into save()
-    # campaign_id = old_campaign['data']['id']
-    data = {
-      'advertiser_id': 162259,
-      'ad_server_id': 9,
-      # # 'end_date': '2018-03-12T09:00:00',
-      # 'goal_type': 'spend',
-      # 'goal_value': '0.0001',
-      'name': 'test campaign on 03/07/2018',
-      # 'service_type': 'SELF',
-      # # 'start_date': '2018-03-11T23:59:00',
-      # 'total_budget': 2,
-    }
-
-    updated_campaign = c.update_campaign(data, 485277)
-
-    # self.assertIn(json.loads(updated_campaign)['data']['name'], 'this is arun suresh\'s test campaign on 03/06/2018')
-
-  def test_get_budget_flights(self):
-    c = campaign.Campaign()
-    assert len(json.loads(c.get_budget_flights(485277))['data']) >= 1
+    def test_get_budget_flights(self):
+        c = campaign.Campaign()
+        budget_flights = json.loads(c.get_budget_flights(485277))['data']
+        assert len(budget_flights) >= 1
